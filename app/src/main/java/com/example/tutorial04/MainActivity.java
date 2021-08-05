15ce93622e08bbe2dd8ae32584d373a68a6db0ae
m-yameen <mmacchhar949@rku.ac.in>
2021-08-05 18:29:38 +0530

    Hiding actionbar

--- app/src/main/java/com/example/tutorial04/MainActivity.java
+++ app/src/main/java/com/example/tutorial04/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.tutorial04;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        ActionBar act = getSupportActionBar();
+        act.hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
